13b761e8166a3d52e1dce962fdd37b0c670ebfbd
msa-sitare <atessude@hotmail.com>
2024-07-11 16:30:22 +0200

    write empty prompt

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissor"];
-  const selectedChoice = Math.floor(Math.random() * choices.length);
-  return choices[selectedChoice];
+  //   const selectedChoice = Math.floor(Math.random() * choices.length);
+  //   return choices[selectedChoice];
+  return choices[0];
 }
-
-getComputerChoice();
 
 function getHumanChoice() {
-    let gamePopup = prompt('Choose between: rock, paper or scissor?');
-    return gamePopup;
+  let gamePopup = prompt("Choose between: rock, paper or scissor?");
+  return gamePopup.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+// WRITE EMPTHY PROMPT OF STATEMENT
 
-function playRound(humanChoice, computerChoice) {
+// let humanScore = 0;
+// let computerScore = 0;
 
-}
+// function playRound(humanChoice, computerChoice) {
+//   if (humanChoice === computerChoice) {
+//     console.log(`It's draw! You both chose ${humanChoice}.`);
+//   } else if (humanChoice === undefined) {
+//     console.log(`You forgot to choose. You need to choose one to play the game!`);
+//   } else {
+//     console.log(`One of you won!`);
+//   }
+// };
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
